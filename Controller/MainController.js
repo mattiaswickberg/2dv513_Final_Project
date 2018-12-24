1410b3a3030028245f304201d0d9983755c91b1c
Mattias Wickberg <mattiaswickberg@gmail.com>
2018-12-24 13:44:06 +0100

    submenu

--- Controller/MainController.js
+++ Controller/MainController.js
@@ -3,17 +3,82 @@ var MainView = require('../View/MainView')
 module.exports = function(){
     var run = true
 
-   // while(run) {
+    while(run) {
         var input = MainView.MainMenu()
         console.log("Input received: " + input)
         if (input === 'q') {
             run = false
         } else if (input === '1') {
-            console.log("Display questions menu")
+            Questions()
 
         } else if (input === '2') {
-            console.log("Display student menu")
+            Student()
+        } else {
+            MainView.InvalidChoice()
         }
+    }
+}
 
-   // }
+var Questions = function() {
+    var run = true
+
+    while(run) {
+    var input = MainView.QuestionsMenu()
+    switch(input) {
+        case 'b':
+        run=false
+        break
+
+        case '1':
+        //TODO
+        break
+
+        case '2':
+        //TODO
+        break
+
+        case '3':
+        //TODO
+        break
+
+        case '4':
+        //TODO
+        break
+
+        default:
+        MainView.InvalidChoice()
+    }
+    }
+}
+
+var Student = function() {
+    var run = true
+
+    while(run) {
+    var input = MainView.StudentsMenu()
+    switch(input) {
+        case 'b':
+        run=false
+        break
+
+        case '1':
+        //TODO
+        break
+
+        case '2':
+        //TODO
+        break
+
+        case '3':
+        //TODO
+        break
+
+        case '4':
+        //TODO
+        break
+
+        default:
+        MainView.InvalidChoice()
+    }
+    }
 }
--- View/MainView.js
+++ View/MainView.js
@@ -1,7 +1,7 @@
 const readlineSync = require('readline-sync')
 
 var MainMenu = function() {
-    console.log('Welcome to QuizBase!')
+    console.log('\n Welcome to QuizBase!')
     console.log('1. View tests and questions')
     console.log('2. View student results')
     console.log('q. Quit')
@@ -9,4 +9,35 @@ var MainMenu = function() {
     return readlineSync.question('Enter your choice: ')
 }
 
+var QuestionsMenu = function() {
+    console.log('\n Tests, Subjects and Questions')
+    console.log('1. List questions by subject')
+    console.log('2. List tests')
+    console.log('3. List questions for specific test')
+    console.log('4. List unused questions')
+    console.log('5. See results for all questions')
+    console.log('6. List results, total and average score per subject')
+    console.log('b. Back to main menu')
+
+    return readlineSync.question('Enter your choice: ')
+}
+
+var StudentsMenu = function() {
+    console.log('\n Students')
+    console.log('1. List students')
+    console.log('2. Get studen with highest total score')
+    console.log('3. Get student with lowest total score')
+    console.log('4. List number of tests taken per student')
+    console.log('b. Back to main menu')
+
+    return readlineSync.question('Enter your choice: ')
+}
+
+var InvalidChoice = function() {
+    console.log('Invalid Choice')
+}
+
 module.exports.MainMenu = MainMenu
+module.exports.QuestionsMenu= QuestionsMenu
+module.exports.StudentsMenu = StudentsMenu
+module.exports.InvalidChoice = InvalidChoice
